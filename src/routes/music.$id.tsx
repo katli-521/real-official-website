227c2a1d0eb273b0d247a7a51f7f2394225bec5d
katli-521 <katli440402@gmail.com>
2026-05-15 00:50:18 +0800

    Delete src/routes/music.$id.tsx

--- src/routes/music.$id.tsx
+++
@@ -1,4 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { EmptySubpageView } from '@/client/views/empty-subpage-view';
-
-export const Route = createFileRoute('/music/$id')({ component: EmptySubpageView });
